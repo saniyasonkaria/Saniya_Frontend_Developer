39184de532bb3889a2e37d3ec3482a080dc5605d
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2023-01-31 21:53:56 +0530

    Added some tailwind css for responsiveness of the banner component.
     Added some tailwind css for responsiveness of the banner component.

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="bg-gray-300 h-96 relative bg-hero bg-no-repeat bg-cover bg-center">
-      <div className="container absolute bottom-6 right-4  mx-auto flex flex-col items-start justify-start p-4 ">
-        <h1 className="text-white text-6xl font-medium ">Welcome</h1>
-        <p className="text-white text-2xl pl-2">to CapsulesX </p>
+    <div className="bg-gray-300 h-64 md:h-96 lg:h-128 relative bg-hero bg-no-repeat bg-cover bg-center">
+    <div className="container absolute bottom-0 right-0  mx-auto flex flex-col items-start justify-start sm:flex-row sm:items-center p-4 ">
+      <div className="sm:w-1/2">
+      <h1 className="text-white text-6xl font-medium ">Welcome</h1>
+      <p className="text-white text-2xl pl-2">to CapsulesX </p>
       </div>
     </div>
+  </div>
   );
 };
 
